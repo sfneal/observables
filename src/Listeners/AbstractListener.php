8c31249761fe8a5f48666e4ed5ebf6a28cb29a8a
Stephen Neal <stephen.neal14@gmail.com>
2021-03-29 14:19:50 -0400

    ADD type hinting to `AbstractListener::handle()`

--- src/Listeners/AbstractListener.php
+++ src/Listeners/AbstractListener.php
@@ -5,10 +5,14 @@ namespace Sfneal\Listeners;
 use Sfneal\Events\AbstractEvent;
 use Sfneal\Queueables\AbstractQueueable;
 
-/**
- * Class Listener.
- * @method void handle(AbstractEvent $event)
- */
+
 abstract class AbstractListener extends AbstractQueueable
 {
+    /**
+     * Handle the event.
+     *
+     * @param AbstractEvent $event
+     * @return void
+     */
+    abstract public function handle(AbstractEvent $event);
 }
