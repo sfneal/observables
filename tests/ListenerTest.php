beb69fcea6576c10d7426b7d0e6b317578ab2090
Stephen Neal <stephen.neal14@gmail.com>
2021-03-29 14:43:41 -0400

    MAKE EventTest & ListenerTest

---
+++ tests/ListenerTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Sfneal\Observables\Tests;
+
+use PHPUnit\Framework\TestCase as PHPUnitTestCase;
+
+class ListenerTest extends PHPUnitTestCase
+{
+    /** @test */
+    public function true_is_true()
+    {
+        $this->assertTrue(true);
+    }
+}
