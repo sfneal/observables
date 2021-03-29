8686fe677abe7fc3cd052f2a4295c2ea092906a2
Stephen Neal <stephen.neal14@gmail.com>
2021-03-29 16:45:32 -0400

    FIX issue with `AbstractListener` type hinting

--- src/Listeners/AbstractListener.php
+++ src/Listeners/AbstractListener.php
@@ -5,15 +5,10 @@ namespace Sfneal\Listeners;
 use Sfneal\Events\AbstractEvent;
 use Sfneal\Queueables\AbstractQueueable;
 
+/**
+ * Class Listener.
+ * @method void handle(AbstractEvent $event)
+ */
 abstract class AbstractListener extends AbstractQueueable
 {
-    // todo: add void return type hinting
-
-    /**
-     * Handle the event.
-     *
-     * @param AbstractEvent $event
-     * @return void
-     */
-    abstract public function handle(AbstractEvent $event);
 }
